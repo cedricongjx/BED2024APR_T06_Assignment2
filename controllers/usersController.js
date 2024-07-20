758f3cbc506ca49a0254ae4f5cd729300b9d0095
cedric.ongjx <cedric.ongjx@gmail.com>
2024-07-20 21:38:31 +0800

    added role for users , updated statistics page to display more indepth statistics and fixed visauls bug

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,16 +4,15 @@ const User = require("../models/user");
 const secretKey = process.env.JWT_SECRET || "your-secret-key";
 
 const createUser = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, role } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.createUser({
       username,
       password: hashedPassword,
+      role
     });
-    res
-      .status(201)
-      .json({ message: "User created successfully", userId: newUser.userId });
+    res.status(201).json({ message: "User created successfully", userId: newUser.userId });
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "Error creating user" });
@@ -31,13 +30,14 @@ const loginUser = async (req, res) => {
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Incorrect password" });
     }
-    const token = jwt.sign({ id: user.userId }, secretKey, { expiresIn: "1h" }); // Ensure user ID is included
+    const token = jwt.sign({ id: user.userId, role: user.role }, secretKey, { expiresIn: "1h" });
     res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     console.error("Error logging in:", error);
     res.status(500).json({ error: "Error logging in" });
   }
 };
+
 
 // Get all users
 const getAllUsers = async (req, res) => {
