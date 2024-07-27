047faa401abac7cb104935e7acf2ff6775cbf581
dylanteo <s10258069@connect.np.edu.sg>
2024-07-27 20:31:52 +0800

    fix

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
-
+ 
 const express = require("express");
 const sql = require("mssql");
 const multer = require("multer");
 const path = require("path");
 const bodyParser = require("body-parser");
-
+ 
 const validateEventDate = require("./middlewares/validateEventDate");
 const authenticateToken = require('./middlewares/authenticateToken');
 const authorizeAdmin = require('./middlewares/authorizeAdmin');
 const validationMiddleware = require('./middlewares/validate');
+const validateEmail = require('./middlewares/validateEmail');
 const validateDonation = require('./middlewares/validateDonation');
-
+ 
 const eventController = require("./controllers/eventController");
 const userController = require("./controllers/userController");
 const categoryController = require("./controllers/categoryController");
 const donationsController = require('./controllers/donationsController');
 const statisticsController = require('./controllers/statisticsController');
-
+ 
 //const authenticateToken = require('./middlewares/authenticateToken');
 //const validationMiddleware = require('./middlewares/validate');
 //const dbConfig = require('./config/dbConfig');
@@ -27,23 +28,24 @@ const newslettersController = require('./controllers/newslettersController');
 const documentaryController = require('./controllers/documentaryController');
 //const validateEmail = require('./middlewares/validateEmail')
 const reviewContoller = require('./controllers/reviewController');
-
+ 
 const feedbackController = require('./controllers/feedbackController');
-
+ 
 const { user } = require('./dbConfig');
-
+ 
 const validateFeedback = require("./middlewares/validateFeedback");
-
-
-
+ 
+const dbConfig = require('./config/dbConfig');
+ 
+ 
 const app = express();
 const port = process.env.PORT || 3000;
-
+ 
 // Middleware to parse JSON and URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+ 
 // Multer configuration for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -53,9 +55,9 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + path.extname(file.originalname));
   }
 });
-
-
-
+ 
+ 
+ 
 // app.post('/api/signup', validationMiddleware.validateSignup, userController.createUser);
 // app.post('/api/login', validationMiddleware.validateLogin, userController.loginUser);
 // app.get('/api/users', userController.getAllUsers);
@@ -65,13 +67,13 @@ const storage = multer.diskStorage({
 // app.post('/api/newsletter', validateEmail, newslettersController.joinNewsletter);
 // app.get('/api/documentary/:id', documentarysController.getDocbyID);
 // app.put('/api/documentary/:id', documentarysController.updateDocByID);
-
+ 
 app.post('/api/donate', authenticateToken, donationsController.createDonation);
-
+ 
 const upload = multer({ storage: storage });
 const testupload = multer({ dest: 'public/images/events' });
 const docUpload = multer({ dest: 'public/images/documentary' });
-
+ 
 // User routes
 app.post('/api/signup', validationMiddleware.validateSignup, userController.createUser);
 app.post('/api/login', validationMiddleware.validateLogin, userController.loginUser);
@@ -79,11 +81,11 @@ app.get('/api/users', authenticateToken, authorizeAdmin, userController.getAllUs
 app.get('/api/users/:id', authenticateToken, authorizeAdmin, validationMiddleware.validateUserIdParam, userController.getUserById);
 app.put('/api/users/:id', authenticateToken, authorizeAdmin, validationMiddleware.validateUserIdParam, validationMiddleware.validateUserUpdate, userController.updateUser);
 app.delete('/api/users/:id', authenticateToken, authorizeAdmin, validationMiddleware.validateUserIdParam, userController.deleteUser);
-
-
+ 
+ 
 // Newsletter routes
 app.post('/api/newsletter', validateEmail, newslettersController.joinNewsletter);
-
+ 
 // Documentary routes
 app.get('/documentary/category/:doccategory', documentaryController.getDocsbyCat);
 app.get('/documentary/search', documentaryController.searchDoc);
@@ -92,7 +94,7 @@ app.put('/documentary/:id', docUpload.single('image'), documentaryController.upd
 app.get('/documentary', documentaryController.getAllDocs);
 app.post('/documentary', docUpload.single('image'), documentaryController.createDoc);
 app.delete('/documentary/:id', documentaryController.deleteDocbyID);
-
+ 
 //Review routes
 app.post('/review/:id', reviewContoller.createReview);
 app.get('/review/:id', reviewContoller.getReviewbyID);
@@ -100,21 +102,21 @@ app.get('/documentary/review/:id', reviewContoller.getReviewsbyDoc);
 app.get('/review/documentary/:id', reviewContoller.createdReview);
 app.get('/review/average/:id', reviewContoller.getAverageStar);
 app.get('/review/total/:id', reviewContoller.getNumberofReviews);
-
-
+ 
+ 
 // Donation routes
 app.post('/api/donate', authenticateToken, validateDonation, donationsController.createDonation);
 app.get('/api/top-donors', donationsController.getTopDonors); // Fetch top donors
-
+ 
 // Statistics routes
 app.get('/api/statistics', statisticsController.getStatistics);
 app.get('/api/average-donations', statisticsController.getAverageDonations);
-
-
+ 
+ 
 app.get("/testgetalluserforevent/:id",userController.getUsersForEvent);
-
+ 
 //------EVENTS------//
-
+ 
 app.get("/event", eventController.getAllEvent);
 app.get("/event/:id", eventController.getEventById);
 app.post("/event", testupload.single('image'), validateEventDate, eventController.createEvent);
@@ -126,7 +128,7 @@ app.post("/upload", upload.single('image'), (req, res) => {
 });
 app.get("/latestEvent", eventController.latestEvent);
 app.get("/events/search", eventController.getEventByName);
-
+ 
 app.put('/event/:id', upload.single('image'), eventController.updateEvent);
 app.post("/upload", upload.single('image'), (req, res) => {
   if (!req.file) {
@@ -146,17 +148,17 @@ app.post("/addcategoryforevent",eventController.addCategoryToEvent);
 app.delete("/removeCategoryFromEvent",eventController.removeCategoryFromEvent);
 app.get("/events/category/:id",eventController.getEventsByCategory);
 app.get("/getCategoryForEvent/:id",eventController.getCategoryForEvent);
-
+ 
 app.put("/event/:id", eventController.updateEvent);
-
+ 
 // User with event routes
 app.get("/userwithevent", userController.getAllUserWithEvents);
 app.get("/userwithevent/:id", userController.getUserWithEventsById);
-
+ 
 // Event with category routes
 app.get("/eventWithCategory", eventController.getEventsWithCategories);
 app.get("/eventWithCategory/:id", eventController.detailedEventById);
-
+ 
 // Category routes
 app.get("/category", categoryController.getAllCategories);
 app.get("/category/:id", categoryController.getCategoryById);
@@ -165,21 +167,21 @@ app.delete("/category/:id", categoryController.deleteCategory);
 app.post("/addcategoryforevent", eventController.addCategoryToEvent);
 app.delete("/removeCategoryFromEvent", eventController.removeCategoryFromEvent);
 app.get("/events/category/:id", eventController.getEventsByCategory);
-
+ 
 // Feedback routes
-
-
+ 
+ 
 // Static file routes
-
+ 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'public/images/event')));
 app.post("/testadduser",userController.registerUserEvent);
 app.delete("/testremoveuser",userController.removeUserFromEvent);
 app.post("/testcheck",userController.isUserRegisteredForEvent);
-
+ 
 //Feedback
 app.put("/feedback/response",authenticateToken,feedbackController.editResponse);
-
+ 
 app.get("/feedback/name",authenticateToken,feedbackController.getFeedbackByName);
 app.get("/feedback",authenticateToken,feedbackController.getAllFeedback);
 app.get("/feedback/notverified",authenticateToken,feedbackController.getAllNotVerifiedFeedback);
@@ -194,12 +196,12 @@ app.delete("/feedback/delete/:id",authenticateToken,feedbackController.deleteFee
 app.post("/feedback/verified",validateFeedback.validateJustification,authenticateToken,feedbackController.addJustification);
 app.get("/feedback/response/:id",authenticateToken,feedbackController.getResponse);
 app.get("/feedback/categorycount",authenticateToken, feedbackController.getFeedbackCountByAllCategory);
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
 // Start the server and connect to the database
 app.listen(port, async () => {
   try {
@@ -210,10 +212,11 @@ app.listen(port, async () => {
     process.exit(1);
   }
 });
-
+ 
 // Handle graceful shutdown
 process.on('SIGINT', async () => {
   console.log('Shutting down gracefully...');
   await sql.close();
   process.exit(0);
 });
+ 
